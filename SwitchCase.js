d089658df879d4e1520253047c3cf99de94c53c0
Rudra Arindom Niloy <159453082+nil3939@users.noreply.github.com>
2025-02-22 20:25:16 +0600

    Add SwitchCase.js

---
+++ SwitchCase.js
@@ -0,0 +1,14 @@
+let day = new Date().getDay();
+switch (day) {
+    case 0:
+        console.log("It's Sunday!");
+        break;
+    case 1:
+        console.log("It's Monday!");
+        break;
+    case 2:
+        console.log("It's Tuesday!");
+        break;
+    default:
+        console.log("It's another day!");
+}
